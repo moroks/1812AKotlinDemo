4e8cadc79ac6f19624b94ae5a628cec176a6ba46
liuchang <moroks@163.com>
2021-06-18 10:00:48 +0800

    修改了一点儿

--- app/src/main/java/com/huluobo/lc/a1812akotlindemo/MainActivity.kt
+++ app/src/main/java/com/huluobo/lc/a1812akotlindemo/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//做了一点儿修改
     }
 }
